f5089cb949e44520bf7e2d92257813213cd02694
mahmoud <mahmoudabdelkareem.info@gmail.com>
2025-05-01 17:51:26 +0300

    Craete SIM manager Page (HTML, CSS and JavaScript

---
+++ JS/Sim.js
@@ -0,0 +1,7 @@
+
+document.getElementById("addSimBtn").onclick = function() {
+  document.getElementById("simModal").classList.remove("hidden");
+};
+document.getElementById("cancelSimModal").onclick = function() {
+  document.getElementById("simModal").classList.add("hidden");
+};
