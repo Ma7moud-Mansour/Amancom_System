d2c07cabb52170e1ddd85821f90e837fe0fa3f20
mahmoud <mahmoudabdelkareem.info@gmail.com>
2025-05-01 17:57:20 +0300

    Create Device Manager page (HTML & CSS & JS)

---
+++ JS/Device.js
@@ -0,0 +1,7 @@
+document.getElementById("addDeviceBtn").onclick = function () {
+    document.getElementById("deviceModal").classList.remove("hidden");
+  };
+  document.getElementById("cancelDeviceModal").onclick = function () {
+    document.getElementById("deviceModal").classList.add("hidden");
+  };
+  
